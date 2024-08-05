85f4e7339c3d3e937eaaf0fc2ce05c1ee2e38a35
Minenhle Ncapai <minipai64@gmail.com>
2024-08-05 10:45:19 +0200

    Add code comments

--- Module_9R_StudentNo_Classcode_Group_Name-Surname_SDF09_R-main/Module_9R_StudentNo_Classcode_Group_Name-Surname_SDF09_R-main/index.js
+++ Module_9R_StudentNo_Classcode_Group_Name-Surname_SDF09_R-main/Module_9R_StudentNo_Classcode_Group_Name-Surname_SDF09_R-main/index.js
@@ -10,10 +10,12 @@ let player = {
 }
 
 let cards = []
-let sum = 0
-let hasBlackJack = false
+let sum = 0 // Sum of the card values
+let hasBlackJack = false // CheckS if player has Blackjack
 let isAlive = false
-let message = ""
+let message = "" // Message to display to the player
+
+// Getting references to HTML elements to update the UI
 let messageEl = document.getElementById("message-el")
 let sumEl = document.getElementById("sum-el")
 let cardsEl = document.getElementById("cards-el")
@@ -21,6 +23,7 @@ let playerEl = document.getElementById("player-el")
 
 playerEl.textContent = player.name + ": $" + player.chips
 
+// Function to generate a random card value between 1 and 13
 function getRandomCard() {
     let randomNumber = Math.floor( Math.random()*13 ) + 1
     if (randomNumber > 10) {
@@ -32,34 +35,36 @@ function getRandomCard() {
     }
 }
 
+// Function to start the game
 function startGame() {
     isAlive = true
-    let firstCard = getRandomCard()
-    let secondCard = getRandomCard()
+    let firstCard = getRandomCard() // Draw first card
+    let secondCard = getRandomCard() // Draw second card
     cards = [firstCard, secondCard]
     sum = firstCard + secondCard
     renderGame()
 }
 
 function renderGame() {
-    cardsEl.textContent = "Cards: "
+    cardsEl.textContent = "Cards: "  // Reset the cards display
     for (let i = 0; i < cards.length; i++) {
-        cardsEl.textContent += cards[i] + " "
+        cardsEl.textContent += cards[i] + " " // Display each card
     }
     
-    sumEl.textContent = "Sum:" + sum
-    if (sum <= 20) {
+    sumEl.textContent = "Sum:" + sum  // Display the sum of the cards
+    if (sum <= 20) { // If sum is less than or equal to 20, prompt for a new card
         message = "Do you want to draw a new card?"
     } else if (sum === 21) {
-        message = "You've got Blackjack!🏆"
+        message = "You've got Blackjack!🏆" 
         hasBlackJack = true
-    } else {
+    } else {  // If sum is greater than 21, player is out of the game
         message = "You're out of the game!"
         isAlive = false
     }
-    messageEl.textContent = message
+    messageEl.textContent = message // Display the message to the player
 }
 
+// Function to draw a new card
 function newCard() {
     if (isAlive === true && hasBlackJack === false) {
         let card = getRandomCard()
